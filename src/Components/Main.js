c561141addaa21c0ae85758a44f15784a9f876a1
jasperxu1233 <jasperhaojie@gmail.com>
2021-10-17 18:23:27 -0400

    Updated

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { Row, Col } from "antd";
 import axios from "axios";
+import { NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY } from "../constants";
 import SatSetting from "./SatSetting";
 import SatelliteList from "./SatelliteList";
 import WorldMap from "./WorldMap";
-import { NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY } from "../constants";
 
 class Main extends Component {
     constructor() {
@@ -12,14 +12,22 @@ class Main extends Component {
         this.state = {
             satInfo: null,
             isLoadingList: false,
+            satList : null,
+            setting : null
         };
     }
-    showNearbySatellite = (setting) => {
+
+    showNearbySatellite = (setting) => {  //得到用户输入的数据并且存储再setting
+        this.setState({
+            isLoadingList: true,  //设置loadingList的状态，等待satellite list拿到后台数据并且被加载出来
+            setting: setting,  //讲填入的卫星参数写入setting
+        });
         this.fetchSatellite(setting);
     };
 
+
     fetchSatellite = (setting) => {
-        const {latitude, longitude, elevation, altitude} = setting;
+        const {latitude, longitude, elevation, altitude} = setting; //解构填入的卫星参数
         const url = `/api/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
 
         this.setState({
@@ -30,7 +38,7 @@ class Main extends Component {
             .then(response => {
                 console.log(response.data)
                 this.setState({
-                    satInfo: response.data,
+                    satInfo: response.data, //将后台拿到的卫星数据存储到satInfo里面
                 })
             })
             .catch(error => {
@@ -44,21 +52,27 @@ class Main extends Component {
             })
     }
 
-    showMap = () => {
-        console.log("show on the map");
+    showMap = (selected) => {  //选中的卫星作存储到satList
+        this.setState((preState) => ({
+           ...preState,
+           satList : [...selected],
+        }))
     };
 
     render() {
-        const { satInfo } = this.state;
+        const { isLoadingList, satInfo, satList, setting } = this.state;
         return (
             <Row className="main">
                 <Col span={8} className="left-side">
                     <SatSetting onShow={this.showNearbySatellite} />
-                    <SatelliteList satInfo={satInfo} onShowMap={this.showMap} isLoad={this.state.isLoadingList}
+                    <SatelliteList
+                        isLoad={isLoadingList}  //传入isLoadingList作为书否将卫星显示出来的参数
+                        satInfo={satInfo}       //后台得到的卫星数据 传入satInfo
+                        onShowMap={this.showMap}
                     />
                 </Col>
                 <Col span={16} className="right-side">
-                    <WorldMap />
+                    <WorldMap satData={satList} observerData={setting} />
                 </Col>
             </Row>
         );
--- src/Components/SatelliteList.js
+++ src/Components/SatelliteList.js
@@ -4,13 +4,13 @@ import satellite from "../assets/images/satellite.svg";
 
 class SatelliteList extends Component {
     state = {
-        selected : [],
+        selected : [],  //selected数组来表示选中的卫星有哪些
     }
 
     onChange = (e) => { //传入一个e
         const { dataInfo, checked } = e.target;  //点一下得到checkbox的状态以及信息
-        const { selected } = this.state;
-        const list = this.addOrRemove(dataInfo, checked, selected);
+        const { selected } = this.state;  //将所有的selected数组里面的卫星信息解构出来放进这个selected里面
+        const list = this.addOrRemove(dataInfo, checked, selected); //讲 checkbox选择的卫星信息，是否选择，和所有选择的卫星放入函数参数
         this.setState({
             selected: list
         })
@@ -19,22 +19,24 @@ class SatelliteList extends Component {
     addOrRemove = (item, status, list) => {
         const found = list.some((entry) => entry.satid === item.satid);
         if(status && !found){ //如果用户选中，切选的那个不在已选中的里面则加进去
-            list.push(item)
+            list.push(item)  //从数组中添加
         }
 
         if(!status && found){//如果用户取消，切选的那个在已选中的里面则取消掉
-            list = list.filter( entry => {
+            list = list.filter( entry => {    //从数组中删除
                 return entry.satid !== item.satid;
             });
         }
         return list;
-
     }
 
+    onShowSatMap = () =>{
+        this.props.onShowMap(this.state.selected);
+    }
 
     render() {
-        const satList = this.props.satInfo ? this.props.satInfo.above : []; //全部的卫星数据
-        const { isLoad } = this.props;  //解构拿到isLoad的数据。
+        const satList = this.props.satInfo ? this.props.satInfo.above : []; //全部的卫星数据存放在satList里面
+        const { isLoad } = this.props;  //解构拿到isLoad的数据,来判断是否要加载。
         const { selected } = this.state;
 
         return (
@@ -55,10 +57,10 @@ class SatelliteList extends Component {
                         className="sat-list"
                         itemLayout="horizontal"
                         size="small"
-                        dataSource={satList}
+                        dataSource={satList}   //所有的卫星信息在satList里面。
                         renderItem={item => (  //展示出所有卫星的信息
                             <List.Item
-                                actions={[<Checkbox dataInfo={item} onChange={this.onChange}/>]}
+                                actions={[<Checkbox dataInfo={item} onChange={this.onChange}/>]} //onChange是否选择一个卫星。选中或者取消
                             >
                                 <List.Item.Meta
                                     avatar={<Avatar size={50} src={satellite} />}
